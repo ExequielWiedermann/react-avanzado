145a92f228070bd3b891d569459e3c8e134591df
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-25 23:12:08 -0300

    Cambiando el render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-ReactDom.render('Hola soy Redner', document.body);
+ReactDom.render('Hola soy Redner',
+document.getElementById('app'));
 
