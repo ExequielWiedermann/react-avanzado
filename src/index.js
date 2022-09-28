b8f5aac79c73e918cc0bce284fd0347a93f8442c
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-27 22:55:20 -0300

    Cambiando comentario de la etiqueta en index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-ReactDom.render(<h1>'Hola soy Redner'</h1>,
+ReactDom.render(<h1>'Sigo preparando la clase 
+    de react avanzado'</h1>,
 document.getElementById('app'));
 
