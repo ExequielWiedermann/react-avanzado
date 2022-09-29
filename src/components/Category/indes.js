fc34fdb28089322bc4fb25fca9a7bb6d9b5bbb05
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-29 17:08:46 -0300

    Creando un componente

---
+++ src/components/Category/indes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DEFAULT_IMAGE = 'https://i.imgur.com/dJA0Hpl.jpg'
+
+export const Category = (
+    {cover = DEFAULT_IMAGE, 
+        path, emoji = '?'}) => (
+    <a href={path}>
+        <img src={cover}/>
+        {emoji}
+    </a>
+)
