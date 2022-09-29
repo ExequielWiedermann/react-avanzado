aadcb90ee991588764522cc8205aa1d094673266
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-09-29 17:04:50 -0300

    Cambiando la forma para usar CSS

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const App = () => (
+    <h1>Sigo preparando la clase
+    de react avanzado</h1>
+)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { App } from './App'
 
-ReactDom.render(<h1>'Sigo preparando la clase
-    de react avanzado'</h1>,
-document.getElementById('app'))
+ReactDom.render(<App />, document.getElementById('app'))
