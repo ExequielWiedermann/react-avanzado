e70b4a86133a0d61072246e9487ca739c8c25be4
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-10-08 17:48:14 -0300

    Configurando el useState y useEffect

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
-import { List, Item } from './style'
-import { categories } from '../../../api/db.json'
+
+import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(function () {
+    window.fetch('https://petgram-server.midudev.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
   return (
-        <List>
-            {
-                // eslint-disable-next-line react/jsx-key
-                // Se puede hacer el <Category {...category} /> con
-                // <Category cover={category.cover} emoji={categoy.emoji} />
-                [categories].map(category => <Item key= {category.id}> <Category {...category} /> </Item>)
-            }
-        </List>
+    <List>
+      {
+        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+      }
+    </List>
   )
 }
