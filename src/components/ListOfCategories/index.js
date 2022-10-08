8cfeeb560ebb4887d0ee9f61ce87265d31cf79dd
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-10-08 09:51:01 -0300

    Consumiendo por la api

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Category } from '../Category'
 import { List, Item } from './style'
+import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
   return (
         <List>
             {
                 // eslint-disable-next-line react/jsx-key
-                [1, 2, 3, 4].map(category => <Item key= {category}> <Category/> </Item>)
+                // Se puede hacer el <Category {...category} /> con
+                // <Category cover={category.cover} emoji={categoy.emoji} />
+                [categories].map(category => <Item key= {category.id}> <Category {...category} /> </Item>)
             }
         </List>
   )
