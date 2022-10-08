08a32d7ad115640398ce0874a111cdca1599caa5
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-10-08 09:37:55 -0300

    Cambiando la etiqueta en List

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Category } from '../Category'
+import { List, Item } from './style'
 
 export const ListOfCategories = () => {
   return (
-        <ul>
+        <List>
             {
                 // eslint-disable-next-line react/jsx-key
-                [1, 2, 3, 4].map(category => <li key= {category}> <Category/> </li>)
+                [1, 2, 3, 4].map(category => <Item key= {category}> <Category/> </Item>)
             }
-        </ul>
+        </List>
   )
 }
---
+++ src/components/ListOfCategories/style.js
@@ -0,0 +1,11 @@
+import styled from 'style-components'
+
+export const List = styled.ul`
+    display: flex;
+    overflow: scroll;
+    width: 100%;
+`
+
+export const Item = styled.li`
+    padding: 0 8px;
+`
