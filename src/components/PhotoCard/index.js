48fd7ab8427c38d804a54a9ab001ce68cb38c187
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-10-11 09:00:09 -0300

    Creando detail en los pages

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -7,9 +7,10 @@ import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 
+import { Link } from '@reach/router'
+
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-// eslint-disable-next-line react/prop-types
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
   const key = `like-${id}`
@@ -19,20 +20,18 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
     <Article ref={element}>
       {
         show && <Fragment>
-          <a href={`/?detail=${id}`}>
+          <Link to={`/detail/${id}`}>
             <ImgWrapper>
               <Img src={src} />
             </ImgWrapper>
-          </a>
+          </Link>
 
           <ToggleLikeMutation>
             {
               (toggleLike) => {
                 const handleFavClick = () => {
-                  // eslint-disable-next-line object-curly-newline
                   !liked && toggleLike({ variables: {
                     input: { id }
-                  // eslint-disable-next-line object-curly-newline
                   } })
 
                   setLiked(!liked)
