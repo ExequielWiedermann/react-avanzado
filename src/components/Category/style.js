bb40ffbf5d8f1c0cbcfc559eaf90d0419a38c8c5
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-10-11 01:01:05 -0300

    Cambiando link para evitar recargar

--- src/components/Category/style.js
+++ src/components/Category/style.js
@@ -1,12 +1,14 @@
 import styled from 'styled-components'
+import { Link as LinkRouter } from '@reach/router'
 
-export const Anchor = styled.a`
+export const Link = styled(LinkRouter)`
   display: flex;
   flex-direction: column;
   text-align: center;
   text-decoration: none;
   width: 75px;
 `
+
 export const Image = styled.img`
   border: 1px solid #ddd;
   box-shadow: 0px 10px 14px rgba(0, 0, 0, .2);
