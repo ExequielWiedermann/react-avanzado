9e723a6db7f6708d0a63b851d7eea320da9696fc
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-10-11 09:10:32 -0300

    Creando el navbar

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Nav } from './styles'
+
+export const NavBar = () => {
+    return (
+        <nav>
+            <button>Home</button>
+            <button>Favs</button>
+            <button>User</button>
+        </nav>
+    )
+}
---
+++ src/components/NavBar/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Nava = styled.nav`
+    align-items: center;
+    background: #fcfcfc;
+    border-top: 1px solid #e0e0e0;
+    bottom: 0;
+    display: flex;
+    height: 50px;
+    justify-content: space-around;
+    left: 0;
+    margin: 0 auto;
+    max-width: 500px;
+    position: fixed;
+    richt: 0;
+    width: 100px;
+    z-index: 1000;
+`
