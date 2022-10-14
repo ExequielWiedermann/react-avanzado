047a5a5dfa1d62aeae398729b21de35f7a109ca1
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-10-14 16:46:55 -0300

    Cambiando el diseño de la app

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,16 +1,26 @@
-import React, { Fragment } from 'react'
-import Context from '../Context'
-import { UserForm } from '../components/UserForm'
+import React, { Fragment } from "react";
+import Context from "../Context";
+import { UserForm } from "../components/UserForm";
+import { RegisterMutation } from "../container/RegisterMutation";
 
 export const NotRegisteredUser = () => (
   <Context.Consumer>
-    {
-      ({ activateAuth }) => {
-        return <Fragment>
-          <UserForm title='Registrarse' onSubmit={activateAuth} />
-          <UserForm title='Iniciar sesión' onSubmit={activateAuth} />
+    {({ activateAuth }) => {
+      return (
+        <Fragment>
+          <RegisterMutation>
+            {function (register) {
+              const onSubmit = ({ email, password }) => {
+                const input = { email, password };
+                const variables = { input };
+                register({ variables }).then(activateAuth);
+              };
+              return <UserForm title="Registrarse" onSubmit={onSubmit} />;
+            }}
+          </RegisterMutation>
+          <UserForm title="Iniciar sesión" onSubmit={activateAuth} />
         </Fragment>
-      }
-    }
+      );
+    }}
   </Context.Consumer>
-)
+);
