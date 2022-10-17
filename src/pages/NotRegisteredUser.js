4f1ac00f7fd3d4102e6266ebc7fa12541faf6ee1
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-10-17 15:58:35 -0300

    Creando el control de estado de carga

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,26 +1,32 @@
-import React, { Fragment } from "react";
-import Context from "../Context";
-import { UserForm } from "../components/UserForm";
-import { RegisterMutation } from "../container/RegisterMutation";
+import React, { Fragment } from 'react'
+import Context from '../Context'
+import { UserForm } from '../components/UserForm'
+import { RegisterMutation } from '../container/RegisterMutation'
 
 export const NotRegisteredUser = () => (
   <Context.Consumer>
-    {({ activateAuth }) => {
-      return (
-        <Fragment>
+    {
+      ({ activateAuth }) => {
+        return <Fragment>
           <RegisterMutation>
-            {function (register) {
-              const onSubmit = ({ email, password }) => {
-                const input = { email, password };
-                const variables = { input };
-                register({ variables }).then(activateAuth);
-              };
-              return <UserForm title="Registrarse" onSubmit={onSubmit} />;
-            }}
+            {
+              (register, { data, loading, error }) => {
+                const onSubmit = ({ email, password }) => {
+                  const input = { email, password }
+                  const variables = { input }
+                  register({ variables }).then(activateAuth)
+                }
+
+                const errorMsg = error && 'El usuario ya existe o hay algún problema.'
+
+                return <UserForm disabled={loading} error={errorMsg} title='Registrarse' onSubmit={onSubmit} />
+              }
+            }
           </RegisterMutation>
-          <UserForm title="Iniciar sesión" onSubmit={activateAuth} />
+
+          <UserForm title='Iniciar sesión' onSubmit={activateAuth} />
         </Fragment>
-      );
-    }}
+      }
+    }
   </Context.Consumer>
-);
+)
