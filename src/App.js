c749f29940e5d78e3e6d7b15cf47de96b3ff377a
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-10-24 10:10:04 -0300

    Cambiando el diseño para utilizar react helmet

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { useContext } from "react";
-import { GlobalStyle } from "./styles/GlobalStyles";
-import { Logo } from "./components/Logo";
-import { NavBar } from "./components/NavBar";
+import React, { useContext } from 'react'
+import { GlobalStyle } from './styles/GlobalStyles'
+import { Logo } from './components/Logo'
+import { NavBar } from './components/NavBar'
 
-import { Home } from "./pages/Home";
-import { Detail } from "./pages/Detail";
-import { Favs } from "./pages/Favs";
-import { User } from "./pages/User";
-import { NotRegisteredUser } from "./pages/NotRegisteredUser";
-import { NotFound } from "./pages/NotFound";
+import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { NotFound } from './pages/NotFound'
 
-import { Router, Redirect } from "@reach/router";
-import { Context } from "./Context";
+import { Router, Redirect } from '@reach/router'
+import { Context } from './Context'
 
 export const App = () => {
-  const { isAuth } = useContext(Context);
+  const { isAuth } = useContext(Context)
 
   return (
     <div>
@@ -22,18 +22,18 @@ export const App = () => {
       <Logo />
       <Router>
         <NotFound default />
-        <Home path="/" />
-        <Home path="/pet/:categoryId" />
-        <Detail path="/detail/:detailId" />
-        {!isAuth && <NotRegisteredUser path="/login" />}
-        {!isAuth && <Redirect from="/favs" to="/login" />}
-        {!isAuth && <Redirect from="/user" to="/login" />}
-
-        <Favs path="/favs" />
-        <User path="/user" />
+        <Home path='/' />
+        <Home path='/pet/:categoryId' />
+        <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect from='/favs' to='/login' />}
+        {!isAuth && <Redirect from='/user' to='/login' />}
+        {isAuth && <Redirect from='/login' to='/' />}
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
 
       <NavBar />
     </div>
-  );
-};
+  )
+}
