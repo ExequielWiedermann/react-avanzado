b07285d7d8e00ba183d4403d655d62dbbdfc9331
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-10-25 00:22:48 -0300

    Cambiando photocard

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,20 +1,14 @@
 import React, { Fragment } from "react";
 import { Article, ImgWrapper, Img } from "./styles";
-
 import { useNearScreen } from "../../hooks/useNearScreen";
-
 import { FavButton } from "../FavButton";
 import { ToggleLikeMutation } from "../../container/ToggleLikeMutation";
-
 import { Link } from "@reach/router";
 import PropTypes from "prop-types";
-
 const DEFAULT_IMAGE =
   "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
-
 export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen();
-
   return (
     <Article ref={element}>
       {show && (
@@ -24,17 +18,11 @@ export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
               <Img src={src} />
             </ImgWrapper>
           </Link>
-
           <ToggleLikeMutation>
             {(toggleLike) => {
               const handleFavClick = () => {
-                toggleLike({
-                  variables: {
-                    input: { id },
-                  },
-                });
+                toggleLike({ variables: { input: { id } } });
               };
-
               return (
                 <FavButton
                   liked={liked}
@@ -49,18 +37,15 @@ export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
     </Article>
   );
 };
-
 PhotoCard.propTypes = {
   id: PropTypes.string.isRequired,
   liked: PropTypes.bool.isRequired,
   src: PropTypes.string.isRequired,
   likes: function (props, propName, componentName) {
     const propValue = props[propName];
-
     if (propValue === undefined) {
       return new Error(`${propName} value must be defined`);
     }
-
     if (propValue < 0) {
       return new Error(`${propName} value must be greater than 0`);
     }
--- src/components/PhotoCard/styled.js
+++ src/components/PhotoCard/styled.js
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 import { fadeIn } from "../../styles/animation";
-
 export const Article = styled.article`
   min-height: 200px;
 `;
-
 export const ImgWrapper = styled.div`
   border-radius: 10px;
   display: block;
@@ -14,7 +12,6 @@ export const ImgWrapper = styled.div`
   position: relative;
   width: 100%;
 `;
-
 export const Img = styled.img`
   ${fadeIn()}
   box-shadow: 0 10px 14px rgba(0, 0, 0, .2);
