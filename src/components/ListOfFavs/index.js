53929cb1df05a1d3c223fbaea3306fcb10150e28
Exequiel Wiedermann <ewiedermann@init-dev.com.ar>
2022-10-25 00:23:54 -0300

    Cambiando el list of favs

--- src/components/ListOfFavs/index.js
+++ src/components/ListOfFavs/index.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { Grid, Image, Link } from './styles'
+import { Grid, Image, Link } from "./styles";
+import PropTypes from "prop-types";
 
-export const ListtOfFavs = ({ favs = [] }) => {
-  return <Grid>
-    {
-        favs.map(fav => <Link key={fav.id} to={`detaild/${fav.id}`}>
-            <Image src={fav.src} />
-        </Link>)
-    }
-  </Grid>
+export const ListOfFavs = ({ favs = [] }) => {
+  return (
+    <Grid>
+      {favs.map((fav) => (
+        <Link key={fav.id} to={`/detail/${fav.id}`}>
+          <Image src={fav.src} />
+        </Link>
+      ))}
+    </Grid>
+  );
+};
+
+ListOfFavs.propTypes = {
+  favs: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      src: PropTypes.string.isRequired,
+    })
+  ),
 };
--- src/components/ListOfFavs/styles.js
+++ src/components/ListOfFavs/styles.js
@@ -20,9 +20,9 @@ export const Grid = styled.div`
   padding-top: 32px;
 `;
 
-export const Imagen = styled.img`
+export const Image = styled.img`
   object-fit: cover;
-  width: 100%;
   height: 100%;
+  width: 100%;
   position: absolute;
 `;
